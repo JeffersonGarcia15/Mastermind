c2381ca5d068b1d6c2f21decdd9853c5b59030f7
JeffersonGarcia15 <JeffersonGarcia1599@gmail.com>
2024-12-12 17:24:14 -0600

    jajg/user_games_routes Adding a get_game_details endpoint.

--- app/api/history_routes.py
+++ app/api/history_routes.py
@@ -3,6 +3,7 @@ from flask_login import current_user
 from app import db
 from app.models.game import Game
 from app.models.match_record import MatchRecord
+from app.models.attempt import Attempt
 from flask_login import login_required
 from app.utils.responses import success_response, error_response
 
@@ -37,5 +38,53 @@ def get_all_games():
             "status": status,
             "score": score
         })
+
+    return success_response(data)
+
+@history_routes.route('/game_details/<string:game_id>', methods=['GET'])
+@login_required
+def get_game_details(game_id):
+    user_id = current_user.id
+    game = Game.query.filter_by(id=game_id).first()
+    if not game:
+        return error_response("Game not found.", 404)
+
+    attempts = Attempt.query.filter_by(game_id=game.id).all()
+    attempts_data = []
+    for a in attempts:
+        # Example hints format: "2 correct positions, 1 correct number"
+        parts = a.hints.split(',') # ["2 correct positions", " 1 correct number"]
+        # parts[0] -> "2 correct positions"
+        # parts[1] -> " 1 correct number"
+        correct_positions_string = parts[0].split()
+        correct_numbers_only_string = parts[1].split()
+        correct_positions = int(correct_positions_string[0])
+        correct_numbers_only = int(correct_numbers_only_string[0])
+        
+        attempts_data.append({
+            "guess": a.guess,
+            "hints": a.hints,
+            "correct_positions": correct_positions,
+            "correct_numbers_only": correct_numbers_only
+        })
+
+    match_record = MatchRecord.query.filter_by(game_id=game.id).first()
+    status = "ongoing"
+    score = None
+    solution = None
+    if match_record:
+        status = match_record.result.value
+        score = match_record.score
+        solution = game.solution
+        
+    data = {
+            "game_id": str(game.id),
+            "difficulty": game.difficulty.value,
+            "created_at": game.created_at.isoformat(),
+            "status": status,
+            "score": score,
+            "solution": solution,
+            "attempts": attempts_data
+        }
 
     return success_response(data)
