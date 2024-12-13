6b6c34abfda60262a0816e5248f3bac72c91fcea
JeffersonGarcia15 <JeffersonGarcia1599@gmail.com>
2024-12-13 11:26:20 -0600

    jajg/UI-auth-and-setup initial setup for pages folder

---
+++ UI/src/pages/LeaderboardPage.jsx
@@ -0,0 +1,7 @@
+export function LeaderboardPage() {
+    return (
+        <div>
+
+        </div>
+    );
+}
---
+++ UI/src/pages/index.jsx
@@ -0,0 +1,6 @@
+export { AuthPage } from "./AuthPage";
+export { GameDetailsPage } from "./GameDetailsPage";
+export { GamePage } from "./GamePage";
+export { HistoryPage } from "./HistoryPage";
+export { HomePage } from "./HomePage";
+export { LeaderboardPage } from "./LeaderboardPage";
